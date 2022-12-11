c7c2fa0be01b65fe3ed93cca3465c686f7460a8f
Colin Marshall <colin.marshall94@gmail.com>
2022-12-11 13:18:24 -0800

    init server files

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
-app.get('/public/assets/notes', (res, req) => 
-    res.sendFile(path.join(__dirname, '/public/assets/notes.html'))
+app.get('/api/notes', (res, req) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
