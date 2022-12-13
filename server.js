4defa79b6fa98cd242cd6304b3bbd2b9d3446cc3
Colin Marshall <colin.marshall94@gmail.com>
2022-12-12 17:11:00 -0800

    added get requests and middleware

--- server.js
+++ server.js
@@ -4,11 +4,55 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const data = require('./db/db.json');
+const fs = require('fs');
+// npm package for creating uuids: https://github.com/uuidjs/uuid
+const { v4: uuidv4 } = require('uuid');
 
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/api/notes', (res, req) => 
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to get notes`);
+    return res.json(data);
+});
+
+// destructures existing note, gets body of existing db.json, then rewrites db.json to include newNote object
+
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            uuid: uuidv4(),
+        };
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) console.error(err);
+            const parsedNotes = JSON.parse(data);
+            parsedNotes.push(newNote);
+            fs.writeFile(
+                './db/db.json', 
+                JSON.stringify(parsedNotes, null, 3), 
+                (err) => err ? console.error(err) : console.info('Note added successfully!')
+            );
+        });
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting note');
+    }
+});
+
+app.get('*',(req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
